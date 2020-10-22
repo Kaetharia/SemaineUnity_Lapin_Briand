86f90eb1aaede327a8d3e1dd31b997583882cdb5
A.Briand <kaetharia@gmail.com>
2020-10-22 13:22:45 +0200

    Un jeu qui marche presque

--- Assets/Scripts/Lapin_Collision.cs
+++ Assets/Scripts/Lapin_Collision.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class Lapin_Collision : MonoBehaviour
 {
-    private Animator animLapin;
-
+    public Animator animLapin;
+    private float invincible = 0.0f;
     void Start()
     {
-        animLapin = GetComponent<Animator>();
+        //animLapin = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        invincible -= Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider collider)
     {
         Debug.Log("Pas Pouf");
 
-        if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player")&& invincible <= 0)
         {
             animLapin.SetTrigger("Tapper");
             Debug.Log("Pouf");
+            invincible = 1.0f;
+            
 
         }
 
